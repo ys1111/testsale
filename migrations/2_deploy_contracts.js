a7760f5f606fd724106f1c4eea6b0885a4ebff8a
ys1111 <y0suke111152@gmail.com>
2018-08-09 19:02:30 +0900

    add .env

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,33 @@
 const YsCoinCrowdsale = artifacts.require('../contracts/YsCoinCrowdsale.sol');
 const YsCoin = artifacts.require('../contracts/YsCoin.sol');
 
-module.exports = function(deployer, network, accounts) {
-  const openingTime = web3.eth.getBlock('latest').timestamp + 2; //two secs in the future
-  const closingTime = openingTime + 86400 * 20;
+module.exports = async function(deployer, network, accounts) {
+  let openingTime;
+  let currentTime; 
+  
+  web3.eth.getBlock('latest', ((error, result) => { 
+     if (!error) {
+       currentTime = result.timestamp
+       return openingTime = result.timestamp + 120
+     } else {
+       console.error(error)
+     }
+   } )); //two secs in the future
+  
   const rate = new web3.BigNumber(1000);
-  const wallet = accounts[1];
-
+  const wallet = accounts[1] || '0xaae900BAe6d741b3cD4aD9a0b07A21413420Ce0c';
   return deployer
       .then(() => {
           return deployer.deploy(YsCoin); 
       })
-      .then(() => {
+      .then(async() => {
+          const closingTime = await openingTime + 86400 * 20;
+          await console.log(openingTime, closingTime, YsCoin.address, rate, wallet)
+          await console.log(currentTime)
           return deployer.deploy(
             YsCoinCrowdsale,
-            openingTime,
-            closingTime,
+            await openingTime,
+            await closingTime,
             rate,
             wallet,
             YsCoin.address
